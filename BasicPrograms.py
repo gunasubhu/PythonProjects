887007ce4acbf861d3f4fb96bbb14e322b6921b4
gunasubhu <gunasubhu@gmail.com>
2024-08-13 03:24:12 -0400

    Update BasicPrograms.py

--- BasicPrograms.py
+++ BasicPrograms.py
@@ -268,19 +268,32 @@ def studentDetails(name,marks):
 
 #Reverse the list
 def reversedlist(num):
- 
     num2 = []
     for i in num:
         print(i,[i],num2)
         num2 = [i] + num2
     print("reversed list : ", num2)
     
-
-
+#Python Program to Split the array and add the first part to the end
+ mylist=[1,2,3,4,5,6,7]
+ firsthalf=mylist[0:4]
+ secondhalf=mylist[4:]
+ addtwolist=(secondhalf + firsthalf)
+ print(addtwolist)
 
 #Reverse the list
 list = [1, 2, 3, 4, 5]
 print("reverse list : ", original_list[::-1])
+
+#Python Program for Find reminder of array multiplication divided by n
+def findremainder(arr,n):
+    mul=1
+    for i in arr:
+        mul=mul*i
+    print(mul)
+    print(mul%n)
+    
+findremainder([100, 10, 5, 25, 35, 14],11)
     
 reversedlist([1, 2, 3, 4, 5,6,7,8,9]) 
 studentDetails("Ranjith",[50,70,65,90,60])        
