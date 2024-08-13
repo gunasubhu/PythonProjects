84eb4f1da7c2229526387c63afd8cdfbe5e16b77
gunasubhu <gunasubhu@gmail.com>
2024-08-13 04:43:44 -0400

    Update List Programs.py

--- List Programs.py
+++ List Programs.py
@@ -1,7 +1,12 @@
 #Python program to interchange first and last elements in a list.
+#Python program to swap two elements in a list
 def swaplist(arr):
-    print(arr[-1:] + arr[0:1])
-
+     arr1=arr[-1:]
+     arr3=arr[1:5]
+     arr2=arr[0:1]
+     print("Original possition in list:",arr[0:6])
+     print("Swap the possition in list values:",  arr1 + arr3 + arr2)
+    
 
     
 swaplist([1,2,3,4,5,6])
