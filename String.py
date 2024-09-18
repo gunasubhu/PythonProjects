693a91adbd7042e772bd7dcf88f28566ced14b7e
gunasubhu <gunasubhu@gmail.com>
2024-09-18 04:46:10 -0400

    Update String.py

--- String.py
+++ String.py
@@ -1,8 +1,14 @@
-#Python Program to Check if a String is Palindrome or Not
+1. #Python Program to Check if a String is Palindrome or Not
 def IsPalindrome(name):
     if (name==name[::-1]):
         print("This is a polindrome:",name)
     else:
         print("This is not a polindrome:",name)
-        
+
+2. #Reverse Words in a Given String in Python
+def reverseWord(name):
+    rev=name[::-1]
+    print("this is reverse string:",rev)
+
+reverseWord("Subramani")
 IsPalindrome("Salem")
