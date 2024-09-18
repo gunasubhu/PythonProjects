1f27829cea4b3b78a76fcc85f9bac89422ea82ad
gunasubhu <gunasubhu@gmail.com>
2024-09-18 03:56:29 -0400

    Update String.py

--- String.py
+++ String.py
@@ -1,1 +1,8 @@
-
+#Python Program to Check if a String is Palindrome or Not
+def IsPalindrome(name):
+    if (name==name[::-1]):
+        print("This is a polindrome:",name)
+    else:
+        print("This is not a polindrome:",name)
+        
+IsPalindrome("Salem")
